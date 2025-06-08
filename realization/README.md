dca73a18aa7e4cc95d98e6173adf4a8f273ca4d3
DaryaEvd <dhfingerstyle@gmail.com>
2025-06-08 23:28:54 +0700

    deleted the 1st ver of realization cause it's bad and useless also it has incorrect configurations of project

--- realization/README.md
+++
@@ -1,8 +0,0 @@
-#HOW TO RUN? 
-
-run  
-`docker compose up --build -d` - чтоб БДшка и сервисы запустились
-
-**
-Если через Intelij Idea, то чтоб БДшку визуально увидеть можно, если прописать
-`jdbc:postgresql://localhost:5432/supervisor`
--- realization/database-service/src/main/java/ru/nsu/fit/evdokimova/database_service/repository/ExperimentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.fit.evdokimova.database_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.nsu.fit.evdokimova.database_service.dto.ExperimentDto;
-
-@Repository
-public interface ExperimentRepository extends JpaRepository<ExperimentDto, Long> {
-}
